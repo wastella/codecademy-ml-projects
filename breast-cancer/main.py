87dcc9e62f32ab98b170d3b4b0e2624ea91367c0
wastella <william.a.stella@gmail.com>
2023-02-28 18:06:13 -0500

    step2

--- breast-cancer/main.py
+++ breast-cancer/main.py
@@ -1,4 +1,7 @@
- from sklearn.datasets import load_breast_cancer
+from sklearn.datasets import load_breast_cancer
 
 breast_cancer_data = load_breast_cancer()
 
+print(breast_cancer_data.data[0])
+
+print(breast_cancer_data.feature_names)
