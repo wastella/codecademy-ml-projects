cccc80312d2953327e684236f60c3c9ab5117762
wastella <william.a.stella@gmail.com>
2023-02-28 18:11:30 -0500

    step5

--- breast-cancer/main.py
+++ breast-cancer/main.py
@@ -3,3 +3,4 @@ from sklearn.model_selection import train_test_split
 
 breast_cancer_data = load_breast_cancer()
 
+train_test_split(breast_cancer_data.data, breast_cancer_data.target, test_size=0.2, random_state=100)
