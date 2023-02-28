8cbc4234aef5a2b0eaff01f99c5d5a05835fa08a
wastella <william.a.stella@gmail.com>
2023-02-28 18:08:22 -0500

    step3

--- breast-cancer/main.py
+++ breast-cancer/main.py
@@ -5,3 +5,6 @@ breast_cancer_data = load_breast_cancer()
 print(breast_cancer_data.data[0])
 
 print(breast_cancer_data.feature_names)
+
+print(breast_cancer_data.target)
+print(breast_cancer_data.target_names)
