3470e749b535ff3eafb113e8da22d21477ae8d32
wastella <william.a.stella@gmail.com>
2023-02-28 18:23:27 -0500

    step10

--- breast-cancer/main.py
+++ breast-cancer/main.py
@@ -7,3 +7,5 @@ breast_cancer_data = load_breast_cancer()
 training_data, validation_data, training_labels, validation_labels = train_test_split(breast_cancer_data.data, breast_cancer_data.target, test_size=0.2, random_state=100)
 
 classifier = KNeighborsClassifier(n_neighbors=3)
+
+classifier.fit(training_data, training_labels)
