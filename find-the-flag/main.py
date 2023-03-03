008ed6b24853668466fb0f5eed8962f314828ead
wastella <william.a.stella@gmail.com>
2023-03-03 16:45:52 -0500

    step1

--- find-the-flag/main.py
+++ find-the-flag/main.py
@@ -3,3 +3,5 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.tree import DecisionTreeClassifier
 import matplotlib.pyplot as plt
+
+df = pd.read_csv("flags.csv", header=0)
