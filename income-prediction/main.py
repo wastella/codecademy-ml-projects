d2f202f02070b18a79eb74a754e92de8d0003d9c
wastella <william.a.stella@gmail.com>
2023-03-04 14:06:45 -0500

    step2

--- income-prediction/main.py
+++ income-prediction/main.py
@@ -7,3 +7,8 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn import tree
 from sklearn.ensemble import RandomForestClassifier
+
+df = pd.read_csv("income.csv", header=0)
+
+
+print(df.columns)
