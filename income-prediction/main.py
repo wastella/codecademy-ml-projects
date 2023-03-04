6acfa054245185627a1984a03757247141bd2b44
wastella <william.a.stella@gmail.com>
2023-03-04 15:30:37 -0500

    step17

--- income-prediction/main.py
+++ income-prediction/main.py
@@ -11,8 +11,11 @@ from sklearn.ensemble import RandomForestClassifier
 df = pd.read_csv("income.csv", header=0, delimiter = ", ")
 
 income_data = df[["income"]]
+
 df["sex-int"] = df["sex"].apply(lambda row: 0 if row == "Male" else 1)
-data = df[["age", "capital-gain", "capital-loss", "hours-per-week", "sex-int"]]
+df["country-int"] = df["native-country"].apply(lambda row: 0 if row == "United-States" else 1)
+
+data = df[["age", "capital-gain", "capital-loss", "hours-per-week", "sex-int", "country-int"]]
 
 train_data, test_data, train_labels, test_labels = train_test_split(data, income_data, random_state=1)
 
